90a079bbbc4842f2b20dbc06343ad01a65e61335
bpx-chain <admin@bpxchain.cc>
2024-05-13 15:07:49 +0200

    BridgedToken: Missing decimals

--- token/BridgedToken.sol
+++ token/BridgedToken.sol
@@ -7,13 +7,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract BridgedToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    uint8 _decimals;
+    
     constructor(string memory name, string memory symbol, uint8 decimals, address bridge)
         ERC20(name, symbol)
         Ownable(bridge)
         ERC20Permit(name)
-    {}
+    {
+        _decimals = decimals;
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+    
+    function decimals() public view override returns (uint8) {
+		return _decimals;
+	}
 }
